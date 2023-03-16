b1768cabaad86d7e49adb00b18727b3424c36d66
Mark Ndungu <markkaris438@gmail.com>
2023-03-16 16:12:24 +0300

    Adds cookie Authentication

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,12 +2,23 @@
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace ContosoPizza.Controllers
 {
     [Route("/[controller]")]
     public class HomeController : Controller
     {
+        private readonly IHttpContextAccessor? _contextAccessor;
+        private readonly HttpContext? _httpContext;
+
+        public HomeController(IHttpContextAccessor httpContext)
+        {
+            _contextAccessor = httpContext;
+            _httpContext = _contextAccessor.HttpContext;
+
+        }
+
         [HttpGet]
         [Authorize(AuthenticationSchemes = OpenIdConnectDefaults.AuthenticationScheme)]
         public ActionResult Get()
@@ -15,7 +26,47 @@ namespace ContosoPizza.Controllers
             return Ok();
         }
 
-        
+        [AllowAnonymous]
+        [HttpGet("/login/sweden")]
+        public async Task<ActionResult> LoginAuthenticationAsync()
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim("usr", "mark"));
+            claims.Add(new Claim("passport_type", "eu"));
+            var identity = new ClaimsIdentity(claims, "cookie");
+            var user = new ClaimsPrincipal(identity);
+            await _httpContext.SignInAsync("cookie", user);
+            return Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("/login")]
+        public async Task<ActionResult> Login()
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim("usr", "mark"));
+            var identity = new ClaimsIdentity(claims, "cookie");
+            var user = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync("cookie", user);
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet("/username")]
+        public ActionResult Username()
+        {
+            var user = _contextAccessor.HttpContext.User;
+            return Ok(user.FindFirst("usr").Value);
+        }
+
+        [Authorize("eu passport")]
+        [HttpGet("/sweden")]
+        public ActionResult Sweden()
+        {
+            return Ok();
+        }
+
+        [AllowAnonymous]
         [HttpPost("/LoginCallback")]
         public async Task<IActionResult> LoginCallback()
         {
--- Program.cs
+++ Program.cs
@@ -1,43 +1,56 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using ContosoPizza.Data;
 using ContosoPizza.Models;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using NuGet.Packaging;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-using Microsoft.AspNetCore.Authentication.Cookies;
 
+/* Use Builder */
 var builder = WebApplication.CreateBuilder(args);
+
+/* Add Services to the builder */
 builder.Services.AddDbContext<ContosoPizzaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ContosoPizzaContext") ?? throw new InvalidOperationException("Connection string 'ContosoPizzaContext' not found.")));
 
-// Add services to the container.
-// Add Controllers with NewtonSoft
+/* DI Controllers */
 builder.Services.AddControllers()
      .AddNewtonsoftJson(options =>
     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
+/* DI HttpContext
+ * HttpContext Allows access to Request and Response blocks
+ * Use IHttpContextAccessor to access HttpContext
+ */
+builder.Services.AddHttpContextAccessor();
 
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+/* DI Swagger */
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAuthentication("Cookies")
-    .AddCookie()
-.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+
+/* DI Authentication */
+builder.Services.AddAuthentication("cookie")
+    .AddCookie("cookie")
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+    {
+        options.Authority = "https://accounts.google.com";
+        options.ClientId = "325233773683-etk0p3gv1glrnfghhhes2vvhev5msksm.apps.googleusercontent.com";
+        options.ClientSecret = "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";
+        options.CallbackPath = "/LoginCallback";
+        options.ResponseType = "code";
+        options.SaveTokens = true;
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+    });
+
+builder.Services.AddAuthorization(builder =>
 {
-    options.Authority = "https://accounts.google.com";
-    options.ClientId = "325233773683-etk0p3gv1glrnfghhhes2vvhev5msksm.apps.googleusercontent.com";
-    options.ClientSecret = "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";
-    options.CallbackPath = "/LoginCallback";
-    options.ResponseType = "code";
-    options.SaveTokens = true;
-    options.Scope.Add("openid");
-    options.Scope.Add("profile");
+    builder.AddPolicy("eu passport", policyBuilder =>
+    {
+        policyBuilder.RequireAuthenticatedUser()
+        .AddAuthenticationSchemes("cookie")
+        .RequireClaim("passport_type", "eu");
+    });
 });
 
 var app = builder.Build();
-
 
 // Run seed data
 using (var scope = app.Services.CreateScope())
@@ -46,18 +59,23 @@ using (var scope = app.Services.CreateScope())
     SeedData.Initialize(services);
 }
 
-// Configure the HTTP request pipeline.
+/* Use Swagger Middleware in Development */
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+/* Use HttpsRedirection Middleware */
 app.UseHttpsRedirection();
 
+/* Use Authentication Middleware */
+app.UseAuthentication();
+/* Use Authorization Middleware */
 app.UseAuthorization();
 
+/* Use Controllers Middleware */
 app.MapControllers();
 
-
+/* Start Application */
 app.Run();
